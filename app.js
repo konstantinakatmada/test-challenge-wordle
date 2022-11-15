29bfca71baea2f6e32f06b7006465e625451c2f6
Konstantina Katmada <konstantinakatmada@gmail.com>
2022-11-15 16:47:40 +0000

    wordle dictionary added

--- app.js
+++ app.js
@@ -1,4 +1,170 @@
-const dictionary = [curl - s 'https://raw.githubusercontent.com/tabatkins/wordle-list/main/words | grep ...'];
+const dictionary = [
+    'power',
+    'waifs',
+    'theta',
+    'nudzh',
+    'arked',
+    'surat',
+    'flash',
+    'empty',
+    'finds',
+    'demos',
+    'riels',
+    'fewer',
+    'jirga',
+    'prows',
+    'kithe',
+    'mambu',
+    'ponts',
+    'recto',
+    'mysid',
+    'usher',
+    'mogas',
+    'joule',
+    'bract',
+    'stots',
+    'pwned',
+    'ambos',
+    'yampa',
+    'ralph',
+    'dozer',
+    'candy',
+    'emics',
+    'jollo',
+    'carvy',
+    'inlay',
+    'cadis',
+    'anode',
+    'oculi',
+    'dervs',
+    'kanas',
+    'doing',
+    'biker',
+    'north',
+    'purin',
+    'pakay',
+    'squee',
+    'tumid',
+    'yerba',
+    'unlit',
+    'fogos',
+    'cells',
+    'mauzy',
+    'cruck',
+    'salts',
+    'parma',
+    'marra',
+    'watts',
+    'bajri',
+    'takas',
+    'wiver',
+    'haats',
+    'stalk',
+    'gunas',
+    'alata',
+    'balds',
+    'wispy',
+    'teils',
+    'habit',
+    'gripe',
+    'peles',
+    'evite',
+    'sophs',
+    'permy',
+    'skeer',
+    'emyde',
+    'redux',
+    'cohen',
+    'women',
+    'hazan',
+    'meris',
+    'staph',
+    'buran',
+    'jones',
+    'hutch',
+    'yuppy',
+    'hosel',
+    'gordo',
+    'mocha',
+    'suits',
+    'miked',
+    'ultra',
+    'sipes',
+    'pinup',
+    'daube',
+    'filer',
+    'recco',
+    'ovals',
+    'flora',
+    'furor',
+    'count',
+    'hoagy',
+    'joist',
+    'lemel',
+    'shwas',
+    'melty',
+    'momme',
+    'scaur',
+    'gynie',
+    'mambo',
+    'twigs',
+    'equid',
+    'foxie',
+    'stone',
+    'trunk',
+    'mbret',
+    'breys',
+    'farer',
+    'hated',
+    'titup',
+    'sleds',
+    'abbey',
+    'glamp',
+    'bludy',
+    'divey',
+    'rhyne',
+    'maned',
+    'ohias',
+    'brail',
+    'visto',
+    'fulls',
+    'gonad',
+    'sprad',
+    'noyau',
+    'aroha',
+    'darga',
+    'motto',
+    'venti',
+    'urned',
+    'lives',
+    'niess',
+    'nests',
+    'mecks',
+    'yacht',
+    'kibei',
+    'wrest',
+    'bosey',
+    'buzzy',
+    'ganks',
+    'laugh',
+    'abuna',
+    'wheel',
+    'kepis',
+    'begat',
+    'senes',
+    'sedan',
+    'monpe',
+    'ghusl',
+    'gamps',
+    'coost',
+    'plume',
+    'dopas',
+    'anise',
+    'stude',
+    'huhus',
+    'kamik',
+    'unsod',
+];
 const state = {
     secret: dictionary[Math.floor(Math.random() * dictionary.length)],
     grid: Array(6)
@@ -7,6 +173,7 @@ const state = {
     currentRow: 0,
     currentCol: 0,
 };
+
 function updateGrid() {
     for (let i = 0; i < state.grid.length; i++) {
         for (let j = 0; j < state.grid[i].length; j++) {
@@ -15,6 +182,7 @@ function updateGrid() {
         }
     }
 }
+
 function drawBox(container, row, col, letter = '') {
     const box = document.createElement('div');
     box.Classname = 'box';
@@ -23,9 +191,11 @@ function drawBox(container, row, col, letter = '') {
     container.appendChild(box);
     return box;
 }
+
 function drawGrid(container) {
     const grid = document.createElement('div');
     grid.className = 'grid';
+
     for (let i = 0; i < 6; i++) {
         for (let j = 0; j < 5; j++) {
             drawBox(grid, i, j);
@@ -33,6 +203,7 @@ function drawGrid(container) {
     }
     container.appendChild(grid);
 }
+
 function registerKeyboardEvents() {
     document.body.onkeydown = (e) => {
         const key = e.key;
@@ -42,20 +213,20 @@ function registerKeyboardEvents() {
                 if (isWordValid(word)) {
                     revealWord(word);
                     state.currentRow++;
-                    state.currentCol = 0; {
-                        else {
-                            alert('Word is not valid.');
-                        }
-                    }
+                    state.currentCol = 0;
+                } else {
+                    alert('This Word is not valid.');
                 }
             }
         }
         if (key === 'Backspace') {
-
+            removeLetter();
         }
         if (isLetter(key)) {
+            addLetter(key);
 
-        } updateGrid();
+        }
+        updateGrid();
     };
 }
 function getCurrentWord() {
@@ -68,9 +239,11 @@ function isWordValid(word) {
 function revealWord(guess) {
     const row = state.currentRow;
     const animation_duration = 500; //ms
+
     for (let i = 0; i < 5; i++) {
         const box = document.getElementById(`box${row}${i}`);
         const letter = box.textContent;
+
         setTimeout(() => {
             if (letter === state.secret[i]) {
                 box.classList.add('right');
@@ -79,41 +252,48 @@ function revealWord(guess) {
             } else {
                 box.classList.add('empty');
             }
+
         }, ((i + 1) * animation_duration) / 2);
+
         box.classList.add('animated');
         box.style.animationDelay = `${(i * animation_duration) / 2}ms`;
     }
-
     const isWinner = state.secret === guess;
     const isGameOver = state.currentRow === 5;
+
     setTimeout(() => {
         if (isWinner) {
             alert('Good Job!');
         } else if (isGameOver) {
-            alert(`Not this time...The word was ${state.secret}`);
+            alert(`Not this time...The word was ${state.secret}.`);
         }
-    },3 * animation_duration);
-    function isLetter(key) {
-        return key.length === 1 && key.match(/[a-z]/i);
-    }
-    function addLetter(letter) {
-        if (state.currentCol === 5) return;
-        state.grid[state.currentRow][state.currentCol] = letter;
-        state.currentCol++;
-    }
-    function removeLetter(letter) {
-        if (state.currentCol === 0) return;
-        state.grid[state.currentRow][state.currentCol - 1] = '';
-        state.currentCol--;
-    }
-    function startup() {
-        const game = document.getElementById('game');
-        drawGrid(game);
+    }, 3 * animation_duration);
+}
 
-        // state.grid = Array(6)
-        //.fill()
-        //.map(() => Array(5).fill('K'));//test filling
-        registerKeyboardEvents();
-        console.log(state.secret);
-    }
-    startup();
+
+
+function isLetter(key) {
+    return key.length === 1 && key.match(/[a-z]/i);
+}
+function addLetter(letter) {
+    if (state.currentCol === 5) return;
+    state.grid[state.currentRow][state.currentCol] = letter;
+    state.currentCol++;
+}
+function removeLetter(letter) {
+    if (state.currentCol === 0) return;
+    state.grid[state.currentRow][state.currentCol - 1] = '';
+    state.currentCol--;
+}
+function startup() {
+    const game = document.getElementById('game');
+    drawGrid(game);
+
+    //state.grid = Array(6);
+    //fill();
+    //map(() => Array(5).fill('K'));//test filling
+    registerKeyboardEvents();
+    //console.log(state.secret);
+}
+
+startup();
